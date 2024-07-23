19f853164b8cd6e5f9487b0f44e4e25bc5bc662d
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-07-23 14:14:44 -0700

    refactored

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-x = parseInt(prompt('Enter Number Grade (Please enter a whole number)'))
-
-if (x >= 90 && x <= 100) {
-  y = 'A'
-} else if (x <= 89 && x >= 80) {
-  y = 'B'
-} else if (x <= 79 && x >= 70) {
-  y = 'C'
-} else if (x <= 69 && x >= 60) {
-  y = 'D'
-} else if (x <= 59 && x >= 50) {
-  y = 'F'
-} else {
-  alert('Error! Please a valid number (0 - 100).')
-}
-
-console.log(
-  'You have a(n) ' +
-    x +
-    '%, which means you have earned a(n) ' +
-    y +
-    ' in class!',
-)
+function main() {
+  let grade = parseInt(document.getElementById('grade').value);
 
 
+  if (grade >= 90 && grade <= 100) {
+    letterGrade = 'A'
+  } else if (grade <= 89 && grade >= 80) {
+    letterGrade = 'B'
+  } else if (grade <= 79 && grade >= 70) {
+    letterGrade = 'C'
+  } else if (grade <= 69 && grade >= 60) {
+    letterGrade = 'D'
+  } else if (grade <= 59 && grade >= 50) {
+    letterGrade = 'F'
+  } else {
+    alert('Error! Please a valid number (0 - 100).')
+  }
+
+  const results = document.getElementById('results');
+  results.innerHTML = `You have a ${grade}%, which means you have earned a(n) ${letterGrade} in class!`
+
+}  
